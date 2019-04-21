3203373b8b88e547108b7bce233bd202cc77d277
Thraxus <mornadak@hotmail.com>
2019-04-21 07:54:18 -0400

    Initial new AI/Bot setup complete.

---
+++ EEM-531659576/Data/Scripts/Thraxus/Bots/Utilities/Structs.cs
@@ -0,0 +1,14 @@
+﻿namespace Eem.Thraxus.Bots.Utilities
+{
+	public struct BotOrphan
+	{
+		public long MyParentId;
+		public long MyGrandParentId;
+
+		public BotOrphan(long myParentId, long myGrandParentId)
+		{
+			MyParentId = myParentId;
+			MyGrandParentId = myGrandParentId;
+		}
+	}
+}
