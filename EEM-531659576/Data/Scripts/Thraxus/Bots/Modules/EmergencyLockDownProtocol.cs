fec747366afeb7345521afb44049d0455a14cb8d
Thraxus <mornadak@hotmail.com>
2019-06-05 05:43:40 -0400

    Plugged in doors completely, need to validate air vents.  Gravity Gens still on the todo.

--- EEM-531659576/Data/Scripts/Thraxus/Bots/Modules/EmergencyLockDownProtocol.cs
+++ EEM-531659576/Data/Scripts/Thraxus/Bots/Modules/EmergencyLockDownProtocol.cs
@@ -5,18 +5,19 @@ using Eem.Thraxus.Common.DataTypes;
 using Eem.Thraxus.Common.Utilities.StaticMethods;
 using Sandbox.Common.ObjectBuilders;
 using Sandbox.Game.Entities;
-using Sandbox.ModAPI;
+using Sandbox.ModAPI.Ingame;
 using Sandbox.ModAPI.Interfaces;
 using SpaceEngineers.Game.ModAPI;
 using VRage.ModAPI;
+using IMyDoor = Sandbox.ModAPI.IMyDoor;
 using IMyEntityIngame = VRage.Game.ModAPI.Ingame.IMyEntity;
+using IMyLargeTurretBase = Sandbox.ModAPI.IMyLargeTurretBase;
+using IMyTerminalBlock = Sandbox.ModAPI.IMyTerminalBlock;
 
 namespace Eem.Thraxus.Bots.Modules
 {
 	public class EmergencyLockDownProtocol : LogBaseEvent, IDisposable
 	{
-		private bool AlertEnabled;
-
 		private enum CubeType
 		{
 			AirVent, Door, Turret, GravityGenerator
@@ -55,8 +56,6 @@ namespace Eem.Thraxus.Bots.Modules
 			}
 		}
 
-		private readonly List<GridTurrets> _gridTurretSettings;
-
 		private struct GridTurrets
 		{
 			public readonly IMyLargeTurretBase Turret;
@@ -77,29 +76,76 @@ namespace Eem.Thraxus.Bots.Modules
 			}
 		}
 
-		private readonly Dictionary<CubeType, object> _warTimeSettings = new Dictionary<CubeType, object>
-		{
-			{CubeType.Turret, new TurretSettings(true, true, true, false, true, false, true, true, 800) },
-			{CubeType.AirVent, new AirVentSettings(true, false) }
-		};
 
-		//private readonly Dictionary<IMyLargeTurretBase, TurretSettings> _archivedTurretSettings;
-		//private readonly List<IMyLargeTurretBase> _turretList;
+		private struct DoorSettings
+		{
+			public readonly bool Enabled;
+
+			public DoorSettings(bool enabled)
+			{
+				Enabled = enabled;
+			}
+		}
+
+		private struct GridDoors
+		{
+			public readonly IMyDoor Door;
+			public readonly DoorSettings WarTimeSettings;
+			public readonly DoorSettings PeaceTimeSettings;
+
+			public GridDoors(IMyDoor door, DoorSettings warTimeSettings, DoorSettings peaceTimeSettings)
+			{
+				Door = door;
+				WarTimeSettings = warTimeSettings;
+				PeaceTimeSettings = peaceTimeSettings;
+			}
+
+		}
 
 		private struct AirVentSettings
 		{
 			public readonly bool Depressurize;
-			public readonly bool PressurizationEnabled;
 
-			public AirVentSettings(bool depressurize, bool pressurizationEnabled)
+			public AirVentSettings(bool depressurize)
 			{
 				Depressurize = depressurize;
-				PressurizationEnabled = pressurizationEnabled;
+			}
+
+			/// <inheritdoc />
+			public override string ToString()
+			{
+				return $"{Depressurize}";
 			}
 		}
 
-		private readonly MyCubeGrid _thisGrid;
+		private struct GridAirVents
+		{
+			public readonly IMyAirVent AirVent;
+			public readonly AirVentSettings WarTimeSettings;
+			public readonly AirVentSettings PeaceTimeSettings;
 
+			public GridAirVents(IMyAirVent airVent, AirVentSettings warTimeSettings, AirVentSettings peaceTimeSettings)
+			{
+				AirVent = airVent;
+				WarTimeSettings = warTimeSettings;
+				PeaceTimeSettings = peaceTimeSettings;
+			}
+
+			/// <inheritdoc />
+			public override string ToString()
+			{
+				return $"{AirVent.EntityId} | {PeaceTimeSettings} | {WarTimeSettings}";
+			}
+		}
+
+		private readonly Dictionary<CubeType, object> _warTimeSettings = new Dictionary<CubeType, object>
+		{
+			{CubeType.Turret, new TurretSettings(true, true, true, false, true, false, true, true, 800) },
+			{CubeType.AirVent, new AirVentSettings(true) },
+			{CubeType.Door, new DoorSettings(false) }
+		};
+
+		
 		public void Init()
 		{
 			int turrets = 0;
@@ -138,6 +184,34 @@ namespace Eem.Thraxus.Bots.Modules
 					IMyDoor door = myCubeBlock as IMyDoor;
 					if (door != null)
 					{
+						//Actions: OnOff | Toggle block On/ Off
+						//Actions: OnOff_On | Toggle block On
+						//Actions: OnOff_Off | Toggle block Off
+						//Actions: ShowOnHUD | Show on HUD On / Off
+						//Actions: ShowOnHUD_On | Show on HUD On
+						//Actions: ShowOnHUD_Off | Show on HUD Off
+						//Actions: Open | Open / Closed
+						//Actions: Open_On | Open
+						//Actions: Open_Off | Closed
+						door.OnDoorStateChanged += delegate(IMyDoor myDoor, bool b)
+						{
+							if (myDoor.OwnerId != _gridOwnerId) return;
+							WriteToLog("OnDoorStateChanged", $"{myDoor.CustomName} {b}", LogType.General);
+							if (!b && _alertEnabled)
+								door.Enabled = false;
+						};
+						
+						_gridDoorSettings.Add(
+							new GridDoors(
+								door,
+								(DoorSettings)_warTimeSettings[CubeType.Door],
+								new DoorSettings(door.Enabled)
+								)
+							);
+						
+						//door.SetValueBool("Open", true);
+						//WriteToLog("EmergencyLockDownProtocol", $"{door.EntityId} | {door.CustomName}", LogType.General);
+						//PrintTerminalActions(door);		
 						++doors;
 						//WriteToLog("EmergencyLockDownProtocol", $"Found a door! {myCubeBlock.GetType()} {++doors}", LogType.General);
 						StaticMethods.AddGpsLocation($"{CubeType.Door.ToString()} {doors}", door.GetPosition());
@@ -146,6 +220,23 @@ namespace Eem.Thraxus.Bots.Modules
 					IMyGravityGeneratorBase generatorBase = myCubeBlock as IMyGravityGeneratorBase;
 					if (generatorBase != null)
 					{
+						//Actions: OnOff | Toggle block On/ Off
+						//Actions: OnOff_On | Toggle block On
+						//Actions: OnOff_Off | Toggle block Off
+						//Actions: ShowOnHUD | Show on HUD On / Off
+						//Actions: ShowOnHUD_On | Show on HUD On
+						//Actions: ShowOnHUD_Off | Show on HUD Off
+						//Actions: IncreaseWidth | Increase Field width
+						//Actions: DecreaseWidth | Decrease Field width
+						//Actions: IncreaseHeight | Increase Field height
+						//Actions: DecreaseHeight | Decrease Field height
+						//Actions: IncreaseDepth | Increase Field depth
+						//Actions: DecreaseDepth | Decrease Field depth
+						//Actions: IncreaseGravity | Increase Acceleration
+						//Actions: DecreaseGravity | Decrease Acceleration
+
+						WriteToLog("EmergencyLockDownProtocol", $"{generatorBase.EntityId} | {generatorBase.CustomName}", LogType.General);
+						PrintTerminalActions(generatorBase);
 						++gravityGenerators;
 						//WriteToLog("EmergencyLockDownProtocol", $"Found a Gravity Generator! {myCubeBlock.GetType()} {++gravityGenerators}", LogType.General);
 						StaticMethods.AddGpsLocation($"{CubeType.GravityGenerator.ToString()} {gravityGenerators}", generatorBase.GetPosition());
@@ -153,9 +244,17 @@ namespace Eem.Thraxus.Bots.Modules
 
 					IMyAirVent vent = myCubeBlock as IMyAirVent;
 					if (vent == null) continue;
+					
+					AirVentSettings archivedSettings = new AirVentSettings(
+						vent.Depressurize
+					);
+
+					_gridAirVentSettings.Add(new GridAirVents(vent, (AirVentSettings)_warTimeSettings[CubeType.AirVent], archivedSettings));
+
 					++airvents;
 					StaticMethods.AddGpsLocation($"{CubeType.AirVent.ToString()} {airvents}", vent.GetPosition());
 				}
+				
 				WriteToLog("EmergencyLockDownProtocol", $"Total Found - Turrets: {turrets} | Air Vents: {airvents} | Doors: {doors} | Gravity Generators: {gravityGenerators}", LogType.General);
 				foreach (GridTurrets gridTurretSettings in _gridTurretSettings)
 					WriteToLog("EmergencyLockDownProtocol", $"{gridTurretSettings}", LogType.General);
@@ -169,17 +268,32 @@ namespace Eem.Thraxus.Bots.Modules
 
 		public void EnableAlert()
 		{
-			if (AlertEnabled) return;
+			if (_alertEnabled) return;
+			WriteToLog("EnableAlert", $"Loading Wartime Settings...", LogType.General);
+
+			for (int index = _gridDoorSettings.Count - 1; index >= 0; index--)
+			{
+				IMyDoor gridDoor = _gridDoorSettings[index].Door;
+				if (!gridDoor.InScene || gridDoor.OwnerId != _gridOwnerId)
+				{
+					_gridDoorSettings.RemoveAtFast(index);
+					continue;
+				}
+				
+				gridDoor.SetValueBool("Open", false);
+				if (gridDoor.Status == DoorStatus.Closed)
+					gridDoor.Enabled = _gridDoorSettings[index].WarTimeSettings.Enabled;
+			}
+
 			for (int index = _gridTurretSettings.Count - 1; index >= 0; index--)
 			{
 				IMyLargeTurretBase turretBase = _gridTurretSettings[index].Turret;
-				if (!turretBase.InScene)
+				if (!turretBase.InScene || turretBase.OwnerId != _gridOwnerId)
 				{
 					_gridTurretSettings.RemoveAtFast(index);
 					continue;
 				}
-
-				WriteToLog("EnableAlert", $"{turretBase.EntityId} - Loading Wartime Settings...", LogType.General);
+				
 				turretBase.Enabled = _gridTurretSettings[index].WarTimeSettings.Enabled;
 				turretBase.SetValueBool("TargetMeteors", _gridTurretSettings[index].WarTimeSettings.TargetCharacters);
 				turretBase.SetValueBool("TargetLargeShips", _gridTurretSettings[index].WarTimeSettings.TargetLargeShips);
@@ -191,28 +305,43 @@ namespace Eem.Thraxus.Bots.Modules
 				turretBase.SetValueFloat("Range", _gridTurretSettings[index].WarTimeSettings.Range);
 			}
 
-			AlertEnabled = true;
-		}
 
-		private void PrintTerminalActions(IMyEntity block)
-		{
-			IMyTerminalBlock myTerminalBlock = block as IMyTerminalBlock;
-			if (myTerminalBlock == null) return;
-			List<ITerminalAction> results = new List<ITerminalAction>();
-			myTerminalBlock.GetActions(results);
-			foreach (ITerminalAction terminalAction in results)
+			for (int index = _gridAirVentSettings.Count - 1; index >= 0; index--)
 			{
-				WriteToLog("TurretControls", $"Actions: {terminalAction.Id} | {terminalAction.Name}", LogType.General);
+				IMyAirVent airVent = _gridAirVentSettings[index].AirVent;
+				if (!airVent.InScene || airVent.OwnerId != _gridOwnerId)
+				{
+					_gridAirVentSettings.RemoveAtFast(index);
+					continue;
+				}
+
+				airVent.Enabled = _gridAirVentSettings[index].WarTimeSettings.Depressurize;
 			}
+
+			_alertEnabled = true;
+			WriteToLog("EnableAlert", $"Wartime Settings Loaded...", LogType.General);
 		}
 
 		public void DisableAlert()
 		{
-			if (!AlertEnabled) return;
+			if (!_alertEnabled) return;
+
+			for (int index = _gridDoorSettings.Count - 1; index >= 0; index--)
+			{
+				IMyDoor gridDoor = _gridDoorSettings[index].Door;
+				if (!gridDoor.InScene || gridDoor.OwnerId != _gridOwnerId)
+				{
+					_gridDoorSettings.RemoveAtFast(index);
+					continue;
+				}
+
+				gridDoor.Enabled = _gridDoorSettings[index].PeaceTimeSettings.Enabled;
+			}
+
 			for (int index = _gridTurretSettings.Count - 1; index >= 0; index--)
 			{
 				IMyLargeTurretBase turretBase = _gridTurretSettings[index].Turret;
-				if (!turretBase.InScene)
+				if (!turretBase.InScene || turretBase.OwnerId != _gridOwnerId)
 				{
 					_gridTurretSettings.RemoveAtFast(index);
 					continue;
@@ -229,13 +358,52 @@ namespace Eem.Thraxus.Bots.Modules
 				turretBase.SetValueBool("TargetStations", _gridTurretSettings[index].PeaceTimeSettings.TargetStations);
 				turretBase.SetValueFloat("Range", _gridTurretSettings[index].PeaceTimeSettings.Range);
 			}
-			AlertEnabled = false;
+
+			for (int index = _gridAirVentSettings.Count - 1; index >= 0; index--)
+			{
+				IMyAirVent airVent = _gridAirVentSettings[index].AirVent;
+				if (!airVent.InScene || airVent.OwnerId != _gridOwnerId)
+				{
+					_gridAirVentSettings.RemoveAtFast(index);
+					continue;
+				}
+
+				airVent.Enabled = _gridAirVentSettings[index].PeaceTimeSettings.Depressurize;
+			}
+
+			_alertEnabled = false;
 		}
+
+		private void PrintTerminalActions(IMyEntity block)
+		{
+			IMyTerminalBlock myTerminalBlock = block as IMyTerminalBlock;
+			if (myTerminalBlock == null) return;
+			List<ITerminalAction> results = new List<ITerminalAction>();
+			myTerminalBlock.GetActions(results);
+			foreach (ITerminalAction terminalAction in results)
+			{
+				WriteToLog("PrintTerminalActions", $"Actions: {terminalAction.Id} | {terminalAction.Name}", LogType.General);
+			}
+		}
+
+
+		private bool _alertEnabled;
+		private readonly long _gridOwnerId;
+
+
+		private readonly MyCubeGrid _thisGrid;
+		private readonly List<GridTurrets> _gridTurretSettings;
+		private readonly List<GridDoors> _gridDoorSettings;
+		private readonly List<GridAirVents> _gridAirVentSettings;
 
 		public EmergencyLockDownProtocol(MyCubeGrid myCubeGrid)
 		{
 			_thisGrid = myCubeGrid;
+			_gridOwnerId = myCubeGrid.BigOwners[0];
+
 			_gridTurretSettings = new List<GridTurrets>();
+			_gridAirVentSettings = new List<GridAirVents>();
+			_gridDoorSettings = new List<GridDoors>();
 			//_archivedTurretSettings = new Dictionary<IMyLargeTurretBase, TurretSettings>();
 			//_turretList = new List<IMyLargeTurretBase>();
 
